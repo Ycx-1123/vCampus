1cdbafdc8f8758c2c3ff2e0bed0b9ba92b7b7a75
lzy <lzy@DESKTOP-VTC64N7>
2026-08-25 16:36:39 +0800

    定义常量

---
+++ src/vCampus/common/consts/MsgConst.java
@@ -0,0 +1,25 @@
+package vCampus.common.consts;
+
+public final class MsgConst {
+	//====学籍管理type常量====
+	//Department
+	public static final String QUERY_ALL_DEPT = "QUERY_ALL_DEPT";
+    public static final String ADD_DEPT = "ADD_DEPT";
+    public static final String UPDATE_DEPT = "UPDATE_DEPT";
+    public static final String DELETE_DEPT = "DELETE_DEPT";
+    //Clazz
+    public static final String QUERY_CLASS_BY_DEPTID = "QUERY_CLASS_BY_DEPTID";
+    public static final String ADD_CLASS = "ADD_CLASS";
+    public static final String UPDATE_CLASS = "UPDATE_CLASS";
+    public static final String DELETE_CLASS = "DELETE_CLASS";
+    //Student
+    public static final String QUERY_STUDENT_BY_CID = "QUERY_STUDENT_BY_CID";
+    public static final String QUERY_STUDENT_BY_SID = "QUERY_STUDENT_BY_SID";
+    public static final String QUERY_ALL_STUDENT = "QUERY_ALL_STUDENT";
+    public static final String ADD_STUDENT = "ADD_STUDENT";
+    public static final String UPDATE_STUDENT = "UPDATE_STUDENT";
+    public static final String DELETE_STUDENT = "DELETE_STUDENT";
+    
+    private MsgConst(){
+    }
+}
